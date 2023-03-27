543e76a1fd61a7c27c133c7d0865df78616540d7
Ivan Castellanos <ivan@ivant.dev>
2023-03-27 17:29:53 -0500

    preparing for migrations

---
+++ db/config.js
@@ -0,0 +1,16 @@
+const config = require('../config/config');
+
+const USER = encodeURIComponent(config.db_user);
+const PASSWORD = encodeURIComponent(config.db_password);
+const URI = `postgres://${USER}:${PASSWORD}@${config.db_host}:${config.db_port}/${config.db_name}`;
+
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  }
+};
