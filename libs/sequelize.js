d66b1aaa31727b4ceb1f1cba63312be7eb58e7a0
Ivan Castellanos <ivan@ivant.dev>
2023-03-27 14:59:48 -0500

    using sequelize

---
+++ libs/sequelize.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize");
+const config = require("../config/config");
+
+const USER = encodeURIComponent(config.db_user);
+const PASSWORD = encodeURIComponent(config.db_password);
+const URI = `postgres://${USER}:${PASSWORD}@${config.db_host}:${config.db_port}/${config.db_name}`;
+
+const sequelize = new Sequelize(URI, {
+  dialect: "postgres",
+  logging: true,
+});
+
+module.exports = sequelize;
--- services/products.service.js
+++ services/products.service.js
@@ -1,14 +1,12 @@
 const { faker } = require("@faker-js/faker");
 const boom = require("@hapi/boom");
 
-const pool = require("../libs/postgres.pool");
+const sequelize = require("../libs/sequelize");
 
 class ProductsServices {
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on("error", (err) => console.error("Unexpected error on idle client", err));
   }
 
   generate() {
@@ -33,8 +31,8 @@ class ProductsServices {
 
   async find() {
     const query = "SELECT * FROM tasks";
-    const res = await this.pool.query(query);
-    return res.rows;
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
